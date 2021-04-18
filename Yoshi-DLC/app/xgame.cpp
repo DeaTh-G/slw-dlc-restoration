c354abb24f10442ec5b04c7269a3343e512654fb
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2021-04-18 11:44:46 +0200

    Add warp collision

--- Yoshi-DLC/app/xgame.cpp
+++ Yoshi-DLC/app/xgame.cpp
@@ -4,6 +4,8 @@ HOOK(bool, __cdecl, IsDLCStagePurchaseHook, ASLR(0x0095DB10), unsigned int dlc)
 {
     if (dlc == 1)
         return true;
+    if (dlc == 2)
+        return true;
     else
         return originalIsDLCStagePurchaseHook(dlc);
 }
