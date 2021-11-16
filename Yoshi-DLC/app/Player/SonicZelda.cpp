790db48ac26536735ea9ed334150885c4aa5084a
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2021-11-16 20:04:40 +0100

    Add fall back for fixed Link Sonic model

--- Yoshi-DLC/app/Player/SonicZelda.cpp
+++ Yoshi-DLC/app/Player/SonicZelda.cpp
@@ -8,6 +8,8 @@ HOOK(void, __fastcall, SonicZeldaInfoHook, ASLR(0x008F8800), int* a1, int* edx, 
     app::ObjUtil::GetPackFile(&packFile, "Linksonic.pac");
 
     app::ObjUtil::GetModelResource(a1 + 4, "chr_Linksonic_fixed", &packFile);
+    if (!*(a1 + 4))
+        app::ObjUtil::GetModelResource(a1 + 4, "chr_Linksonic", &packFile);
 }
 
 void app::Player::SonicZeldaInfo::Initialize()
