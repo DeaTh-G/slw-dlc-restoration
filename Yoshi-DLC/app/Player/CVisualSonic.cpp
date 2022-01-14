f1011801ea007d9ef64081cf3d38fee612751208
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2022-01-14 02:05:13 +0100

    Fix Crashing issue with colliding options

--- Yoshi-DLC/app/Player/CVisualSonic.cpp
+++ Yoshi-DLC/app/Player/CVisualSonic.cpp
@@ -3,27 +3,51 @@
 HOOK(void, __cdecl, RegisterResourceHook, ASLR(0x008FE300), int* a1, int* a2, int* a3, int a4)
 {
     const char* packFileName = app::ObjUtil::GetStagePackName(*app::Document);
-    if (strncmp(packFileName, "zdlc03", 6) == 0 || LinkSonicPlayType == PlayType::ALWAYS)
-        a4 = 1;
 
-    if (LinkSonicPlayType == PlayType::NEVER)
+    switch (LinkSonicPlayType)
+    {
+    case PlayType::DEFAULT:
+    {
+        if (strncmp(packFileName, "zdlc03", 6) == 0)
+            a4 = 1;
+        else
+            a4 = 0;
+
+        break;
+    }
+    case PlayType::ALWAYS:
+        a4 = 1;
+        break;
+    case PlayType::NEVER:
         a4 = 0;
+        break;
+    }
 
     originalRegisterResourceHook(a1, a2, a3, a4);
 }
 
 HOOK(void, __fastcall, ActivateSubHook, ASLR(0x008FE290), int* a1, int* edx)
 {
-    if (LinkSonicPlayType == PlayType::NEVER)
-    {
-        *(*((*(unsigned int***)(a1 + 4)) + 210) + 6) &= ~0x80000000;
-        originalActivateSubHook(a1, edx);
-        return;
-    }
-
     const char* packFileName = app::ObjUtil::GetStagePackName(*app::Document);
-    if (strncmp(packFileName, "zdlc03", 6) == 0 || LinkSonicPlayType == PlayType::ALWAYS)
+
+    switch (LinkSonicPlayType)
+    {
+    case PlayType::DEFAULT:
+    {
+        if (strncmp(packFileName, "zdlc03", 6) == 0)
+            *(*((*(unsigned int***)(a1 + 4)) + 210) + 6) |= 0x80000000;
+        else
+            *(*((*(unsigned int***)(a1 + 4)) + 210) + 6) &= ~0x80000000;
+
+        break;
+    }
+    case PlayType::ALWAYS:
         *(*((*(unsigned int***)(a1 + 4)) + 210) + 6) |= 0x80000000;
+        break;
+    case PlayType::NEVER:
+        *(*((*(unsigned int***)(a1 + 4)) + 210) + 6) &= ~0x80000000;
+        break;
+    }
 
     originalActivateSubHook(a1, edx);
 }
