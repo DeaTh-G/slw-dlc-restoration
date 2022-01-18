c2e96508ba1978e7839c0643cea7312be2093bba
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2022-01-18 18:54:36 +0100

    Fix Knockback Message

--- Yoshi-DLC/app/xgame/MsgAppeareKnockback.h
+++ Yoshi-DLC/app/xgame/MsgAppeareKnockback.h
@@ -7,13 +7,13 @@ namespace app
         class MsgAppeareKnockback : public fnd::Message
         {
         public:
-            int field_18;
-            int field_1C;
-            csl::math::Vector3 field_20;
-            int field_30;
-            int field_34;
-            int field_38;
-            int field_3C;
+            int field_18{};
+            int field_1C{};
+            csl::math::Vector3 field_20{};
+            int field_30{};
+            int field_34{};
+            int field_38{};
+            int field_3C{};
 
             MsgAppeareKnockback() : Message()
             {
