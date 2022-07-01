b18c87902d1dc9d97055f402bd4f4919fe1a93e5
DeaTh-G <kalapos.gyorgy19990714@gmail.com>
2022-07-01 17:55:05 +0200

    Disable Ring Scatter in "Always Play with Heart System" option

--- slw-dlc-restoration/app/Player/StateUtil.cpp
+++ slw-dlc-restoration/app/Player/StateUtil.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 
+static void* ReturnAddressScatterRingForDamageAlwaysHeartLife = (void*)ASLR(0x008D9FB7);
 static void* ReturnAddressScatterRingForDamage = (void*)ASLR(0x008D9FA6);
 static void* StateUtilLostRingOffset = (void*)ASLR(0x008D9090);
 
@@ -18,12 +19,26 @@ __declspec(naked) void ScatterRingForDamageMidAsmHook()
         push esi
         call [StateUtilLostRingOffset]
         add esp, 8
-        
-        jmp [ReturnAddressScatterRingForDamage]
+
+        jmp[ReturnAddressScatterRingForDamage]
+    }
+}
+
+__declspec(naked) void ScatterRingForDamageAlwaysHeartLifeMidAsmHook()
+{
+    __asm
+    {
+        push esi
+        call app::Player::StateUtil::ReduceHeartLife
+        add esp, 4
+        jmp [ReturnAddressScatterRingForDamageAlwaysHeartLife]
     }
 }
 
 void app::Player::StateUtil::ScatterRingForDamage()
 {
-    WRITE_JUMP(ASLR(0x008D9F9B), &ScatterRingForDamageMidAsmHook);
+    if (IsAlwaysHeartLife)
+        WRITE_JUMP(ASLR(0x008D9F9B), &ScatterRingForDamageAlwaysHeartLifeMidAsmHook)
+    else
+        WRITE_JUMP(ASLR(0x008D9F9B), &ScatterRingForDamageMidAsmHook);
 }
