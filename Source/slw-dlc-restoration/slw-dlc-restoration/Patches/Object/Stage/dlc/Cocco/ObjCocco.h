06d62cb43cb40e8ab93a3780b53d0ff77edd2fb2
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-18 13:45:02 +0200

    Add multiplayer support to ObjCocco

---
+++ Source/slw-dlc-restoration/slw-dlc-restoration/Patches/Object/Stage/dlc/Cocco/ObjCocco.h
@@ -0,0 +1,35 @@
+#pragma once
+
+namespace slw_dlc_restoration
+{
+	class ObjCocco : public app::ObjCocco
+	{
+	public:
+		typedef TiFsmState_t(app::ObjCocco::* BaseState)(const TiFsmEvent_t&);
+
+	public:
+		class CInfo : public app::ObjCocco::CInfo
+		{
+		public:
+			uint PlayerNo{ static_cast<uint>(-1) };
+		};
+
+		uint PlayerNo{ static_cast<uint>(-1) };
+
+		ObjCocco();
+		ObjCocco(const CInfo& in_rCreateInfo);
+		void AddCallback(app::GameDocument& in_rDocument) override;
+		void Update(const app::fnd::SUpdateInfo& in_rUpdateInfo) override;
+		TiFsmState_t StateIdle(const TiFsmEvent_t& in_rEvent);
+		TiFsmState_t StateAttackIn(const TiFsmEvent_t& in_rEvent);
+		TiFsmState_t StateAttackOut(const TiFsmEvent_t& in_rEvent);
+		void SetTargetPlayer();
+		bool IsInCamera() const;
+		void CreateAttackers();
+
+		static ObjCocco* CreateAttacker(const CInfo& in_rCreateInfo)
+		{
+			return new ObjCocco(in_rCreateInfo);
+		}
+	};
+}
