85b44112b13d2ce0bca60f28912cd6ef0e5d6c62
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-18 13:36:22 +0200

    Add option to disable the shadow of ObjYoshiJumpBoard

---
+++ Source/slw-dlc-restoration/slw-dlc-restoration/Patches/Object/Stage/dlc/YoshiJumpBoard/ObjYoshiJumpBoard.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include "ObjYoshiJumpBoard.h"
+
+slw_dlc_restoration::ObjYoshiJumpBoard::ObjYoshiJumpBoard(app::yoshi_jump_board::JumpBoardType in_type) : app::ObjYoshiJumpBoard(in_type)
+{
+
+}
+
+void slw_dlc_restoration::ObjYoshiJumpBoard::AddCallback(app::GameDocument& in_rDocument)
+{
+	app::ObjYoshiJumpBoard::AddCallback(in_rDocument);
+
+	if (auto* pShadowSimple = GetComponent<app::game::GOCShadowSimple>())
+		pShadowSimple->SetVisible(false);
+}
+
+app::CSetObjectListener* slw_dlc_restoration::ObjYoshiJumpBoard::Create_Big()
+{
+	return new ObjYoshiJumpBoard(app::yoshi_jump_board::JumpBoardType::Big);
+}
+
+app::CSetObjectListener* slw_dlc_restoration::ObjYoshiJumpBoard::Create_Small()
+{
+	return new ObjYoshiJumpBoard(app::yoshi_jump_board::JumpBoardType::Small);
+}
