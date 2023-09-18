ed338af858914b70bccdf9a71ebc39058c6a38a3
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-18 13:38:24 +0200

    Add option to play as Link Sonic in multiplayer

---
+++ Source/slw-dlc-restoration/slw-dlc-restoration/Patches/System/Player/Info/PlayerVisualVirtualSonicInfo.cpp
@@ -0,0 +1,54 @@
+#include "pch.h"
+#include "PlayerVisualVirtualSonicInfo.h"
+
+static const char* LINKSONIC_PACKFILE_NAME = (const char*)ASLR(0x00E02C80);
+static const char* LINKSONIC_ASSET_NAME = (const char*)ASLR(0x00E02C90);
+
+HOOK(void, __fastcall, LoadHook, ASLR(0x008F8BA0), app::Player::VirtualSonicInfo* in_pThis, void* edx, app::CObjInfoFileLoader& in_rLoader)
+{
+	originalLoadHook(in_pThis, edx, in_rLoader);
+	
+	// Linksonic's packfile never gets loaded in multiplayer so it needs to be manually loaded.
+	// The archive name is referenced from the game to ensure mod compatibility.
+	// In addition another packfile is loaded for the added alt skin added by the mod.
+	in_rLoader.Load(LINKSONIC_PACKFILE_NAME, 0x4001);
+	in_rLoader.Load("Linksonic2p.pac", 0x4001);
+}
+
+HOOK(void, __fastcall, InitializeHook, ASLR(0x008F8C00), app::Player::VirtualSonicInfo* in_pThis, void* edx, app::GameDocument& in_rDocument)
+{
+	originalInitializeHook(in_pThis, edx, in_rDocument);
+
+	// The models of the two Virtual Sonics for multiplayer get replaced by Link Sonic, and an alt costume for Link Sonic.
+	// The packfile and the asset names for Player 1 are referenced from the game's memory to ensure compatibility with mods.
+	auto packFile = app::ObjUtil::GetPackFile(LINKSONIC_PACKFILE_NAME);
+	in_pThis->Models[0] = app::ObjUtil::GetModelResource(LINKSONIC_ASSET_NAME, packFile);
+	if (CONFIGURATION.ZeldaTweaks.IsLinkSonicFixed)
+	{
+		if (auto model = app::ObjUtil::GetModelResource("chr_Linksonic_fixed", packFile))
+			in_pThis->Models[0] = model;
+	}
+
+	packFile = app::ObjUtil::GetPackFile("Linksonic2p.pac");
+	in_pThis->Models[1] = app::ObjUtil::GetModelResource("chr_Linksonic2p", packFile);
+	if (CONFIGURATION.ZeldaTweaks.IsLinkSonicFixed)
+	{
+		if (auto model = app::ObjUtil::GetModelResource("chr_Linksonic2p_fixed", packFile))
+			in_pThis->Models[1] = model;
+	}
+
+	// The antenna models gets stubbed out for the multiplayer Link Sonics as their looks do not fit
+	// the looks that the option is going for.
+	in_pThis->AntennaModels[0] = { nullptr };
+	in_pThis->AntennaModels[1] = { nullptr };
+}
+
+void slw_dlc_restoration::Player::VirtualSonicInfo::InstallHooks()
+{
+	// The Legend of Zelda Zone DLC Hooks
+	if (CONFIGURATION.ZeldaTweaks.IsVirtualLinkSonic)
+	{
+		INSTALL_HOOK(LoadHook);
+		INSTALL_HOOK(InitializeHook);
+	}
+}
---
+++ Source/slw-dlc-restoration/slw-dlc-restoration/Patches/System/Player/Info/PlayerVisualVirtualSonicInfo.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace slw_dlc_restoration::Player
+{
+	class VirtualSonicInfo
+	{
+	public:
+		static void InstallHooks();
+	};
+}
