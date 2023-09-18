3bcdc03e6824c559cb39ec2c2e4d8793f2f0c0dc
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-18 13:33:59 +0200

    Add option to fix the particle position of ObjEventDrowning

---
+++ Source/slw-dlc-restoration/slw-dlc-restoration/Patches/Object/Stage/dlc/EventDrowning/ObjEventDrowning.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace slw_dlc_restoration
+{
+	class ObjEventDrowning : public app::ObjEventDrowning
+	{
+	public:
+		void AddCallback(app::GameDocument& in_rDocument) override;
+		bool ProcessMessage(app::fnd::Message& in_rMessage) override;
+		bool ProcMsgHitEventCollision(app::xgame::MsgHitEventCollision& in_rMessage);
+	};
+}
