f9cfb1e7f85ed03c07116f992ec9534d2bdbf4e2
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-18 13:31:18 +0200

    Add multiplayer support to ObjChangeTopViewCollision

---
+++ Source/slw-dlc-restoration/slw-dlc-restoration/Patches/Object/Stage/dlc/ChangeTopViewCollision/ObjChangeTopViewCollision.cpp
@@ -0,0 +1,33 @@
+#include "pch.h"
+#include "ObjChangeTopViewCollision.h"
+
+bool slw_dlc_restoration::ObjChangeTopViewCollision::ProcessMessage(app::fnd::Message& in_rMessage)
+{
+	if (PreProcessMessage(in_rMessage))
+		return true;
+
+	switch (in_rMessage.GetType())
+	{
+	case app::xgame::MsgHitEventCollision::MessageID:			return ProcMsgHitEventCollision(static_cast<app::xgame::MsgHitEventCollision&>(in_rMessage));
+	case app::xgame::MsgLeaveEventCollision::MessageID:			return ProcMsgLeaveEventCollision(static_cast<app::xgame::MsgLeaveEventCollision&>(in_rMessage));
+	default:													return app::ObjChangeTopViewCollision::ProcessMessage(in_rMessage);
+	}
+}
+
+bool slw_dlc_restoration::ObjChangeTopViewCollision::ProcMsgHitEventCollision(app::xgame::MsgHitEventCollision& in_rMessage)
+{
+	app::xgame::MsgPLChangeAreaTopView msg{};
+	msg.IsTopView = true;
+
+	app::ObjUtil::SendMessageImmToPlayer(*this, app::ObjUtil::GetPlayerNo(*GetDocument(), in_rMessage.m_Sender), msg);
+	return true;
+}
+
+bool slw_dlc_restoration::ObjChangeTopViewCollision::ProcMsgLeaveEventCollision(app::xgame::MsgLeaveEventCollision& in_rMessage)
+{
+	app::xgame::MsgPLChangeAreaTopView msg{};
+	msg.IsTopView = false;
+
+	app::ObjUtil::SendMessageImmToPlayer(*this, app::ObjUtil::GetPlayerNo(*GetDocument(), in_rMessage.m_Sender), msg);
+	return true;
+}
