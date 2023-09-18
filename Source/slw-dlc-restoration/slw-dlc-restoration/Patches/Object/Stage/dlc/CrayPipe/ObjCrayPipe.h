98dbaab938e0318d1c61f766577b0d71dfb04b4d
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-18 13:33:04 +0200

    Add multiplayer support to CrayPipe and CrayPipeExit

---
+++ Source/slw-dlc-restoration/slw-dlc-restoration/Patches/Object/Stage/dlc/CrayPipe/ObjCrayPipe.h
@@ -0,0 +1,32 @@
+#pragma once
+
+namespace slw_dlc_restoration
+{
+	class ObjCrayPipe : public app::ObjCrayPipe
+	{
+	public:
+		typedef TiFsmState_t(app::ObjCrayPipe::* BaseState)(const TiFsmEvent_t&);
+
+	public:
+		int NumPlayers{};
+
+		void AddCallback(app::GameDocument& in_rDocument) override;
+		void Update(const app::fnd::SUpdateInfo& in_rUpdateInfo) override;
+		TiFsmState_t StatePipeIn(const TiFsmEvent_t& in_rEvent);
+	};
+
+	class ObjCrayPipeExit : public app::ObjCrayPipeExit
+	{
+	public:
+		typedef TiFsmState_t(app::ObjCrayPipeExit::* BaseState)(const TiFsmEvent_t&);
+
+	public:
+		bool AllPlayersCaught{};
+		int NumPlayers{};
+
+		ObjCrayPipeExit();
+		void AddCallback(app::GameDocument& in_rDocument) override;
+		TiFsmState_t StateIdle(const TiFsmEvent_t& in_rEvent);
+		TiFsmState_t StatePipeOut(const TiFsmEvent_t& in_rEvent);
+	};
+}
