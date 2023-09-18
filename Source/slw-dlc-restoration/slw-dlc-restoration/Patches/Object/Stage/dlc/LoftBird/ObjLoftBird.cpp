070f9aeb47f95cbac3f8f454199a94bc7e1e1512
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-18 13:34:33 +0200

    Fix memory leak caused by ObjLoftBird

---
+++ Source/slw-dlc-restoration/slw-dlc-restoration/Patches/Object/Stage/dlc/LoftBird/ObjLoftBird.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "ObjLoftBird.h"
+
+void slw_dlc_restoration::ObjLoftBird::RemoveCallback(app::GameDocument& in_rDocument)
+{
+	for (auto& light : PointLights)
+	{
+		if (!light.IsValid())
+			continue;
+		
+		app::xgame::MsgKill msg{};
+		SendMessageImmToGameObject(light.Get(), msg);
+	}
+}
---
+++ Source/slw-dlc-restoration/slw-dlc-restoration/Patches/Object/Stage/dlc/LoftBird/ObjLoftBird.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace slw_dlc_restoration
+{
+	class ObjLoftBird : public app::ObjLoftBird
+	{
+	public:
+		void RemoveCallback(app::GameDocument& in_rDocument) override;
+	};
+}
