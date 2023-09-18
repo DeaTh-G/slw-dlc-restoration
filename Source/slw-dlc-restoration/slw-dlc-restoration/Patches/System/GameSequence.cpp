25ca9db5c608850bd8a44053e2eeb7968771054c
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-18 13:37:33 +0200

    Add legacy code for playing the SHC2021 dlc advert video

---
+++ Source/slw-dlc-restoration/slw-dlc-restoration/Patches/System/GameSequence.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include "GameSequence.h"
+
+HOOK(void, __fastcall, CompleteDLCStageHook, ASLR(0x009122C0), app::CGameSequence* in_pThis, void* edx, app::xgame::DlcStageIndex in_index, uint in_unk1, uint in_unk2)
+{
+    if (CONFIGURATION.GlobalTweaks.IsSHC2021Build && !strcmp(in_pThis->m_pGame->GetGameMode()->GetName(), "GameModeStage"))
+    {
+        auto* pGameMode = static_cast<app::GameModeStage*>(in_pThis->m_pGame->GetGameMode());
+        if (!strcmp(pGameMode->pLevelInfo->m_Level, "zdlc02"))
+        {
+            in_pThis->PushMovieEventQueue(327);
+            in_pThis->SeqGotoPlayMovie();
+
+            return;
+        }
+    }
+
+    originalCompleteDLCStageHook(in_pThis, edx, in_index, in_unk1, in_unk2);
+}
+
+void slw_dlc_restoration::CGameSequence::InstallPatches()
+{
+    // General Patches
+    // Replace junk data in the rewards DLC array with the proper values.
+    WRITE_MEMORY(ASLR(0x00E05654), uint, 22);
+    WRITE_MEMORY(ASLR(0x00E05658), uint, 23);
+
+    // Run the for loop for the actual length of the array instead of the trimmed PC version.
+    WRITE_MEMORY(ASLR(0x0090D3FD), byte, 5);
+}
+
+void slw_dlc_restoration::CGameSequence::InstallHooks()
+{
+    // General Hooks
+    INSTALL_HOOK(CompleteDLCStageHook);
+}
---
+++ Source/slw-dlc-restoration/slw-dlc-restoration/Patches/System/GameSequence.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace slw_dlc_restoration
+{
+	class CGameSequence
+	{
+	public:
+		static void InstallPatches();
+		static void InstallHooks();
+	};
+}
